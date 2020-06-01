036d284f80fc2849768550084eaa8f8c7cfa2bfa
Bu Sun Kim <busunkim@google.com>
2020-06-01 21:37:22 +0000

    chore: use f-strings

--- samples/snippets/list_voices.py
+++ samples/snippets/list_voices.py
@@ -33,19 +33,19 @@ def list_voices():
 
     for voice in voices.voices:
         # Display the voice's name. Example: tpc-vocoded
-        print("Name: {}".format(voice.name))
+        print(f"Name: {voice.name}")
 
         # Display the supported language codes for this voice. Example: "en-US"
         for language_code in voice.language_codes:
-            print("Supported language: {}".format(language_code))
+            print(f"Supported language: {language_code}")
 
         ssml_gender = texttospeech.SsmlVoiceGender(voice.ssml_gender)
 
         # Display the SSML Voice Gender
-        print("SSML Voice Gender: {}".format(ssml_gender.name))
+        print(f"SSML Voice Gender: {ssml_gender.name}")
 
         # Display the natural sample rate hertz for this voice. Example: 24000
-        print("Natural Sample Rate Hertz: {}\n".format(voice.natural_sample_rate_hertz))
+        print(f"Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}\n")
 
 
 # [END tts_list_voices]
