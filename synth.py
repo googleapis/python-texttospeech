1474d2b3934b896248b9b802451ab8cbac72fa5c
Bu Sun Kim <busunkim@google.com>
2020-06-25 03:30:49 +0000

    docs: fix dcostringsg

--- synth.py
+++ synth.py
@@ -32,7 +32,7 @@ for version in versions:
 
 # Sphinx interprets `*` as emphasis
 s.replace(
-    ["google/cloud/**/*_client.py", "google/cloud/**/cloud_tts.py"],
+    ["google/cloud/**/client.py", "google/cloud/**/cloud_tts.py"],
     "((en)|(no)|(nb)(cmn)|(yue))-\*",
     "\g<1>-\*",
 )
