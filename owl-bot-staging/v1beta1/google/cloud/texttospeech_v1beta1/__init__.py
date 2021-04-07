c2df31adf9e5910e4e99b6a39fb2e639394ea108
Anthonios Partheniou <partheniou@google.com>
2021-04-06 20:21:05 -0400

    chore: copy files from googleapis-gen f92897093108f24c013e9c15c3be2709417ba5d6

---
+++ owl-bot-staging/v1beta1/google/cloud/texttospeech_v1beta1/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from .services.text_to_speech import TextToSpeechClient
+from .types.cloud_tts import AudioConfig
+from .types.cloud_tts import AudioEncoding
+from .types.cloud_tts import ListVoicesRequest
+from .types.cloud_tts import ListVoicesResponse
+from .types.cloud_tts import SsmlVoiceGender
+from .types.cloud_tts import SynthesisInput
+from .types.cloud_tts import SynthesizeSpeechRequest
+from .types.cloud_tts import SynthesizeSpeechResponse
+from .types.cloud_tts import Timepoint
+from .types.cloud_tts import Voice
+from .types.cloud_tts import VoiceSelectionParams
+
+
+__all__ = (
+    'AudioConfig',
+    'AudioEncoding',
+    'ListVoicesRequest',
+    'ListVoicesResponse',
+    'SsmlVoiceGender',
+    'SynthesisInput',
+    'SynthesizeSpeechRequest',
+    'SynthesizeSpeechResponse',
+    'Timepoint',
+    'Voice',
+    'VoiceSelectionParams',
+'TextToSpeechClient',
+)
